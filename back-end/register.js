725785d1f6e78f66c7ab9dc37a87f9336f1ba201
samuelfilho-dev <snamuelfilho2011@gmail.com>
2023-12-14 18:01:14 -0300

    first commit

---
+++ back-end/register.js
@@ -0,0 +1,17 @@
+function saveData() {
+    // Obtenha os valores dos campos
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // Crie um objeto com os dados
+    const userData = {
+        firstName: firstName,
+        lastName: lastName,
+        username: username,
+        password: password
+    };
+
+    
+}
