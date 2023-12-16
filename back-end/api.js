21c2f7ba883e6d7425b5678b6f342d96d37e7711
samuelfilho-dev <snamuelfilho2011@gmail.com>
2023-12-16 20:27:38 -0300

    feat: create new routes

--- back-end/api.js
+++ back-end/api.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from "cors";
 import bodyParser from "body-parser";
-import {client} from "./database.js";
+import client from "./database.js";
 
 const app = express();
 
@@ -10,26 +10,31 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
 
 app.get('/api/v1/users', async (req, res) => {
-    console.log('Está pegando')
+    try {
+        const conn = await client.connect();
+        const result = await client.query('SELECT * FROM users');
+
+        return res.status(200).json(result.rows);
+    } catch (error) {
+        return res.status(500).send({'message': error.message})
+    } finally {
+        await client.end();
+    }
 })
 
 app.post('/api/v1/users', async (req, res) => {
     const {firstName, lastName, username, password} = req.body;
 
-    await client.connect()
-        .then(() => {
-            return client.query('INSERT INTO users (first_name, last_name, username, password) VALUES ($1, $2, $3, $4) RETURNING *', [firstName, lastName, username, password]);
-        })
-        .then(result => {
-            return res.status(201).json(result.rows[0]);
-        })
-        .catch(error => {
-            res.status(500).send('Erro interno do servidor');
-            console.error('Erro ao salvar usuário:', error);
-        })
-        .finally(async () => {
-            await client.end();
-        });
+    try {
+        const conn = await client.connect();
+        const result = await client.query('INSERT INTO users (first_name, last_name, username, password) VALUES ($1, $2, $3, $4) RETURNING *', [firstName, lastName, username, password]);
+
+        return res.status(201).json(result.rows[0]);
+    } catch (error) {
+        return res.status(500).send({'message': error.message});
+    } finally {
+        await client.end();
+    }
 
 
 })
@@ -39,14 +44,19 @@ app.post('/api/v1/login', async (req, res) => {
 
     const {username, password} = req.body;
 
-    await client.connect();
-    const result = await client.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password]);
-    await client.end();
+    try {
+        const conn = await client.connect();
+        const result = await client.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password]);
+        if (result.rows.length > 0) {
+            return res.status(200).send({'message': 'Login Bem-sucedido'});
+        } else {
+            return res.status(400).send({'message': 'Erro no Login'});
+        }
 
-    if (result.rows.length > 1) {
-        return res.status(200).send({'message': 'Login Bem-sucedido'});
-    } else {
-        return res.status(400).send({'message': 'Erro no Login'});
+    } catch (error) {
+        return res.status(500).send({'message': error.message});
+    } finally {
+        await client.end();
     }
 });
 
--- back-end/database.js
+++ back-end/database.js
@@ -1,10 +1,12 @@
 import pg from 'pg';
 
-export const client = new pg.Client({
+const client = new pg.Client({
     user: 'postgres',
     password: 'senha123',
     database: 'dio_apache',
     port: 5432,
     host: 'localhost',
 })
+
+export default client;
 
